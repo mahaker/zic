9cb806528b4a78bb67bf5882fea8dc6ac654cbe1
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-09-15 09:58:35 +0900

    add Env.getGithubPAT() test.

--- app/test/env.spec.ts
+++ app/test/env.spec.ts
@@ -1,7 +1,7 @@
-jest.mock('fs')
 import * as fs from 'fs'
 import * as Env from '@/env'
 
+jest.mock('fs')
 
 describe('Env#getRepositoryId', () => {
   beforeEach(() => {
@@ -21,5 +21,26 @@ describe('Env#getRepositoryId', () => {
     const repoId = Env.getRepositoryId()
 
     expect(repoId).toBe(12345)
+  })
+})
+
+describe('Env#getGithubPAT', () => {
+  beforeEach(() => {
+    const mocked: jest.Mocked<typeof fs> = fs as any
+    mocked.readFileSync.mockReturnValue(JSON.stringify({'github-pat': 'mocked pat'}))
+  })
+  afterEach(() => {
+    delete process.env.GITHUB_PAT
+  })
+  it('from process.env', () => {
+    process.env.GITHUB_PAT = 'aaa'
+    const pat = Env.getGitHubPAT()
+
+    expect(pat).toBe('aaa')
+  })
+  it('from dev.json', () => {
+    const pat = Env.getGitHubPAT()
+
+    expect(pat).toBe('mocked pat')
   })
 })
